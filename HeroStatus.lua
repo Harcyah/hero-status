920e9bf07689f701ed1b0a9f4bcaecc2080bd6a5
Raph Bonnet <harcyah@gmail.com>
2019-08-25 23:30:32 +0200

    Add equipment set status

--- HeroStatus.lua
+++ HeroStatus.lua
@@ -1,20 +1,42 @@
 
-local function CreateStatusFrame(name, textureFileName, r, g, b, a)
+local ICON_WIDTH = 32;
+local ICON_HEIGHT = 32;
+local X_OFFSET = 20;
+local Y_OFFSET = 20;
+local SPACE = 5;
+
+local INDEX_REPAIR = 0;
+local INDEX_EQUIPMENT = 1;
+
+local function CreateStatusFrame(name, textureFileName, index, r, g, b, a)
 	local frameName = "HeroStatus" .. name .. "Frame";
 	local textureBackgroundName = "HeroStatus" .. name .. "TextureBackground";
 	local textureOverlayName = "HeroStatus" .. name .. "TextureOverlay";
 
 	local frame = CreateFrame("Frame", frameName, UIParent);
-	frame:SetSize(32, 32);
+	frame:SetSize(ICON_WIDTH, ICON_HEIGHT);
+	frame:SetBackdrop({
+		bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+		edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+		tile = true,
+		tileSize = 32,
+		edgeSize = 10,
+		insets = {
+			left = 1,
+			right = 1,
+			top = 1,
+			bottom = 1
+		}
+	});
 	frame:SetBackdropColor(0, 0, 0);
 	frame:SetPoint("CENTER");
 	frame:SetMovable(false);
 	frame:EnableMouse(false);
 	frame:SetPoint("CENTER");
-	frame:SetPoint("BOTTOMLEFT", "UIParent", "BOTTOMLEFT", 20, 20);
+	frame:SetPoint("BOTTOMLEFT", "UIParent", "BOTTOMLEFT", X_OFFSET + (index * (ICON_WIDTH + SPACE)), Y_OFFSET);
 	frame:Show();
 
-	local textureBackground = frame:CreateTexture(textureBackgroundName, "BACKGROUND");
+	local textureBackground = frame:CreateTexture(textureBackgroundName, "ARTWORK");
 	textureBackground:SetTexture(textureFileName);
 	textureBackground:SetAllPoints(frame);
 
@@ -25,8 +47,22 @@ local function CreateStatusFrame(name, textureFileName, r, g, b, a)
 	return frame;
 end
 
-local frameRepairStatusOk = CreateStatusFrame('RepairStatusOk', "Interface\\Icons\\Ability_Repair.blp", 0, 0, 0, 0);
-local frameRepairStatusWarn = CreateStatusFrame('RepairStatusWarn', "Interface\\Icons\\Ability_Repair.blp", 1, 0, 0, 0.25);
+local function CreateRepairStatusFrame()
+	local textureName = "Interface\\Icons\\Ability_Repair.blp"
+	local frameOk = CreateStatusFrame('RepairStatusOk', textureName, INDEX_REPAIR, 0, 0, 0, 0);
+	local frameWarn = CreateStatusFrame('RepairStatusWarn', textureName, INDEX_REPAIR, 1, 0, 0, 0.25);
+	return frameOk, frameWarn
+end
+
+local function CreateEquipmentStatusFrame()
+	local textureName = "Interface\\PaperDollInfoFrame\\UI-EquipmentManager-Toggle.blp"
+	local frameOk = CreateStatusFrame('EquipmentStatusOk', textureName, INDEX_EQUIPMENT, 0, 0, 0, 0);
+	local frameWarn = CreateStatusFrame('EquipmentStatusWarn', textureName, INDEX_EQUIPMENT, 1, 0, 0, 0.25);
+	return frameOk, frameWarn
+end
+
+local frameRepairStatusOk, frameRepairStatusWarn = CreateRepairStatusFrame()
+local frameEquipmentStatusOk, frameEquipmentStatusWarn = CreateEquipmentStatusFrame()
 
 local function IsRepaired()
 	local SLOTS = {
@@ -52,7 +88,7 @@ local function IsRepaired()
 	return true
 end
 
-local function update()
+local function UpdateRepairStatus()
 	if (IsRepaired()) then
 		frameRepairStatusOk:Show();
 		frameRepairStatusWarn:Hide();
@@ -62,18 +98,53 @@ local function update()
 	end
 end
 
-local frame = CreateFrame("Frame", "HeroStatusFrame", UIParent);
-frame:RegisterEvent("PLAYER_ENTERING_WORLD");
-frame:RegisterEvent("UPDATE_INVENTORY_DURABILITY");
+local function IsDefaultEquipmentSetValid()
+	local equipmentSetID = C_EquipmentSet.GetEquipmentSetID("Default");
+	if (equipmentSetID == nil) then
+		return false;
+	end
 
-frame:SetScript("OnEvent", function(self, event, ...)
+	local _, _, _, _, numItems, numEquipped, numInInventory, numLost, numIgnored = C_EquipmentSet.GetEquipmentSetInfo(equipmentSetID);
+	if (numLost > 0) then
+		return false
+	end
+
+	return true
+end
+
+local function UpdateEquipmentStatus()
+	if (IsDefaultEquipmentSetValid()) then
+		frameEquipmentStatusOk:Show();
+		frameEquipmentStatusWarn:Hide();
+	else
+		frameEquipmentStatusOk:Hide();
+		frameEquipmentStatusWarn:Show();
+	end
+end
+
+local eventFrame = CreateFrame("Frame", "HeroStatusFrame", UIParent);
+eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
+eventFrame:RegisterEvent("UPDATE_INVENTORY_DURABILITY");
+eventFrame:RegisterEvent("EQUIPMENT_SETS_CHANGED");
+eventFrame:RegisterEvent("UNIT_INVENTORY_CHANGED");
+
+eventFrame:SetScript("OnEvent", function(self, event, ...)
 
 	if (event == "PLAYER_ENTERING_WORLD") then
-		update()
+		UpdateRepairStatus();
+		UpdateEquipmentStatus();
 	end
 
 	if (event == "UPDATE_INVENTORY_DURABILITY") then
-		update()
+		UpdateRepairStatus();
+	end
+
+	if (event == "EQUIPMENT_SETS_CHANGED") then
+		UpdateEquipmentStatus();
+	end
+
+	if (event == "UNIT_INVENTORY_CHANGED") then
+		UpdateEquipmentStatus();
 	end
 
 end)
