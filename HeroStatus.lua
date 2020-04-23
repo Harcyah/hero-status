cfd7333dc41ffb3e033937278eb5be8f8b4d5f7c
Raphaël Bonnet <harcyah@gmail.com>
2020-04-23 23:15:33 +0200

    Display WorldPvpStatus at startup

--- HeroStatus.lua
+++ HeroStatus.lua
@@ -167,6 +167,18 @@ local function UpdateEquipmentStatus()
 	end
 end
 
+local function DisplayWorldPvpStatus(id)
+	local _, localizedName, isActive, _, startTime = GetWorldPVPAreaInfo(id)
+	DEFAULT_CHAT_FRAME:AddMessage('[world pvp status] ' .. localizedName .. ' active ? ' .. tostring(isActive) .. ', ' .. SecondsToTime(startTime) .. ' seconds', 0.8, 0.8, 0.8);
+end
+
+local function UpdateWorldPvpStatus()
+	local WINTERGRASP = 1;
+	local TOLBARAD = 2;
+	DisplayWorldPvpStatus(WINTERGRASP);
+	DisplayWorldPvpStatus(TOLBARAD);
+end
+
 local eventFrame = CreateFrame("Frame", "HeroStatusFrame", UIParent);
 eventFrame:RegisterEvent("BAG_UPDATE");
 eventFrame:RegisterEvent("EQUIPMENT_SETS_CHANGED");
@@ -195,6 +207,7 @@ eventFrame:SetScript("OnEvent", function(self, event, ...)
 		UpdateRepairStatus();
 		UpdateJunkStatus();
 		UpdateEquipmentStatus();
+		UpdateWorldPvpStatus();
 	end
 
 	if (event == "UNIT_INVENTORY_CHANGED") then
